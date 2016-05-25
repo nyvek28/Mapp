bfe4b738dbfc02f1b49d1a31444f64e087c41eb9
Kev <kevynquiros@gmail.com>
2016-05-24 20:48:58 -0600

    Implemented web sockets with Socket.io

--- server/index.js
+++ server/index.js
@@ -5,7 +5,26 @@ let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let port = 3000;
+let connections = [];
+let users = [];
+
+app.use(express.static('public'))
+
+app.get('/api', function(req, res){
+  res.status(200).send('Hello World!');
+});
+
+io.on('connection', function(socket){
+  connections.push(socket);
+  console.log('Current sockets: ' + connections.length);
+
+  socket.on('disconnect', function(){
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Current sockets: ' + connections.length);
+  });
+
+});
 
 server.listen(port, function(){
-  console.log('Magic happens at port :' + port);
+  console.log('Magic happens at port : ' + port);
 });
